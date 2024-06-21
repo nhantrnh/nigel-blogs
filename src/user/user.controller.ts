af5f0e8f233767203e91bdbfb6a496c1a82b7932
21120105 <nhantrnh03@gmail.com>
2024-06-21 17:43:44 +0700

    pagination, search and filter

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { FilterUserDto } from './dto/filter-user.dto';
 
 @Controller('users')
 export class UserController {
@@ -13,8 +14,9 @@ export class UserController {
     
     @UseGuards(AuthGuard)
     @Get()
-    findAll():Promise<User[]>{
-        return this.userService.findAll();
+    findAll(@Query() query: FilterUserDto):Promise<User[]>{
+        console.log(query);
+        return this.userService.findAll(query);
     }
 
     @UseGuards(AuthGuard)
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,19 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
-import { DeleteResult, Repository, UpdateResult } from 'typeorm';
+import { DeleteResult, Like, Repository, UpdateResult } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { FilterUserDto } from './dto/filter-user.dto';
 @Injectable()
 export class UserService {
     [x: string]: any;
 
     constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
-    async findAll(): Promise<User[]> {
-        return await this.userRepository.find({
+    async findAll(query: FilterUserDto): Promise<any> {
+        const items_per_page = query.items_per_page ? parseInt(query.items_per_page) : 5;
+        const page = Number(query.page)|| 1;
+        // skip: bo qua bao nhieu items
+        // take: lay bao nhieu items
+        const skip = (page -1) * items_per_page;
+        //total: tong so items
+        const keyword = query.search ? query.search : '';
+        const[res, total] = await this.userRepository.findAndCount({
+            where: [
+                {firstName: Like(`%${keyword}%`)},
+                {lastName: Like(`%${keyword}%`)},
+                {email: Like(`%${keyword}%`)}
+            ],
+            order: {createAt: 'DESC'},
+            take: items_per_page,
+            skip: skip,
             select: ['id', 'firstName', 'lastName', 'email', 'status', 'createAt', 'updateAt']
-        });
+        })
+        const lastPage = Math.ceil(total/items_per_page);
+        const nextPage = page + 1 > lastPage ? null : page + 1;
+        const prevPage = page - 1 <= 0 ? null : page - 1;
+
+        return {
+            data: res,
+            total,
+            currentPage: page,
+            nextPage,
+            prevPage,
+            lastPage
+        }
     }
 
     async findOne(id: number): Promise<User> {
